f2386422e100d51e0a1370cd3c7ed317ff6be32a
Samy <shagun.sinha030310@gmail.com>
2026-09-11 22:42:23 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "jee-pwa-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./manifest.json"
+      ])
+    )
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
